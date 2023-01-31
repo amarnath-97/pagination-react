caabcfb105c82a4d2dacb9c4a5cf47a08f31e6d8
F22 Labs <amarnath@f22labs.com>
2023-01-31 12:05:19 +0530

    Pagination with react

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+    data && data.products && setProducts(data.products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const selectedPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length/10 &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+
+  return (
+    <div className="App">
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+            return (
+              <span className="product__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <div className="pagination">
+          <span onClick={() => selectedPageHandler(page - 1)}
+          className={page > 1 ? "" : "page__disabled"}
+          >Prev</span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                onClick={() => selectedPageHandler(i + 1)}
+                className={page === i + 1 ? "page__selected" : ""}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span onClick={() => selectedPageHandler(page + 1)} className={page < products.length/10 ? "" : "page__disabled"}>Next</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
